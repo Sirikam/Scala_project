1e9f267a50182f0d0fdf5f2c98ca6ef053b7905b
Sirikam <ben.dik@live.no>
2017-10-25 13:37:48 +0200

    adds multiple exception

--- Project/task1-project/src/main/scala/Account.scala
+++ Project/task1-project/src/main/scala/Account.scala
@@ -2,14 +2,21 @@ import exceptions._
 
 class Account(var initialBalance: Double, val uid: Int = Bank getUniqueId) {
   def withdraw(amount: Double): Unit = {
+	if (amount < 0) {
+		throw new IllegalAmountException("cant subtract negative numbers")
+	}
+
   	if (amount > initialBalance) {
-  		throw new NoSufficientFundsException("exception!")
+  		throw new NoSufficientFundsException("Not enough money left in the account")
   	}
   	else {
   		initialBalance = initialBalance - amount
   	}
   } 
   def deposit(amount: Double): Unit = {
+  	if (amount < 0) {
+		throw new IllegalAmountException("cant subtract negative numbers")
+	}
   	initialBalance = initialBalance + amount
   } 
   def getBalanceAmount: Double = initialBalance;  
