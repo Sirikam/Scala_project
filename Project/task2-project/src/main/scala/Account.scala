0ba664a5d7d8517221e9de17bf93ba6732fa5f97
Sirikam <ben.dik@live.no>
2017-10-25 14:51:00 +0200

    adds a .gitignore to task2, updates the account class with the solution from task 1

--- Project/task2-project/src/main/scala/Account.scala
+++ Project/task2-project/src/main/scala/Account.scala
@@ -7,13 +7,28 @@ class Account(val bank: Bank, initialBalance: Double) {
   val balance = new Balance(initialBalance)
   val uid = bank.generateAccountId
 
-  def withdraw(amount: Double): Unit = ??? // Like in part 1
-  def deposit(amount: Double): Unit = ??? // Like in part 1
-  def getBalanceAmount: Double = ??? // Like in part 1
+  def withdraw(amount: Double): Unit = {
+  	this.synchronized{
+  		if (amount < 0) {
+		throw new IllegalAmountException("cant subtract negative numbers")
+		} 
+		if (amount > initialBalance) {
+  			throw new NoSufficientFundsException("Not enough money left in the account")
+  		}
+  		initialBalance = initialBalance - amount
+  	}
+  } 
+  def deposit(amount: Double): Unit = {
+  	this.synchronized{
+  		if (amount < 0) {
+		throw new IllegalAmountException("cant subtract negative numbers")
+		}
+  		initialBalance = initialBalance + amount
+  	}
+  } 
+  def getBalanceAmount: Double = initialBalance;  
 
   def transferTo(account: Account, amount: Double) = {
     bank addTransactionToQueue (this, account, amount)
   }
-
-
 }
