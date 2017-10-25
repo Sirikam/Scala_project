e8be4da6b74f562903d698cb1b038bc1bbc46e86
Sirikam <ben.dik@live.no>
2017-10-25 12:56:54 +0200

    account updates

--- Project/task1-project/src/main/scala/Account.scala
+++ Project/task1-project/src/main/scala/Account.scala
@@ -1,11 +1,9 @@
-class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
+class Account(var initialBalance: Double, val uid: Int = Bank getUniqueId) {
   def withdraw(amount: Double): Unit = {
   	initialBalance = initialBalance - amount
   } 
   def deposit(amount: Double): Unit = {
   	initialBalance = initialBalance + amount
   } 
-  def getBalanceAmount: Double = {
-  	println(initialBalance)
-  } 
+  def getBalanceAmount: Double = initialBalance;  
 }
